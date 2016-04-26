f95e4548bd3ad38467a0e095a69ed1190c765cf3
vdublunatic <jclark4@mail.greenriver.edu>
2016-04-25 18:15:32 -0700

    Ignored some files

--- catWords/client/main.js
+++ catWords/client/main.js
@@ -3,25 +3,97 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.player.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+//fields
+var words = [];
+var dictionary = [
+
+    //categories of words
+
+    {
+        category: "cars",
+        carWords: ["engine", "tire", "starter", "alternator", "radiator", "transmission", "reverse", "drive", "golf",
+        "camaro", "prelude"]
+
+    }
+];
+var players = [
+    {
+        playerId: 1,
+        playerName: "Player1",
+        chosenWords: [],
+        score: 0,
+        highScore: 0,
+        winning: false
+    },
+
+    {
+        playerId: 2,
+        playerName: "Player2",
+        chosenWords: [],
+        score: 0,
+        highScore: 0,
+        winning: false
+    },
+
+    {
+        playerId: 3,
+        playerName: "Player3",
+        chosenWords: [],
+        score: 0,
+        highScore: 0,
+        winning: false
+    },
+
+    {
+        playerId: 4,
+        playerName: "Player4",
+        chosenWords: [],
+        score: 0,
+        highScore: 0,
+        winning: false
+    },
+];
+
+//Players
+Template.player.onCreated(function () {
+
+    Session.set('players', players);
+    console.log("onCreated() for players called");
+
 });
 
 Template.player.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+
 });
 
 Template.player.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+
+    'click button' : function () {
+
+        //prevent form from refreshing page
+        event.preventDefault();
+
+        //get our players current word
+        var currentWord = $('#wordSubmit').val();
+
+        //remove the current word from the submit form
+        $('#wordSubmit').val("");
+
+        //add the word to the players list of chosen words
+        players[this.playerId].chosenWords.push(currentWord);
+
+        Set.session('word', words);
+        
+    }
+
 });
 
-Template.word.onCreated(function helloOnCreated() {
+
+//Word in the middle
+Template.word.onCreated(function () {
+
+    Session.set('word', words);
+    console.log("onCreated() for words called");
 
 });
 
